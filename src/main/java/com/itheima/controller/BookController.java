282b57842ed9c589f9280b8c3ae448a48422b48e
LLT <LLT@QQ.COM>
2022-05-18 13:24:39 +0800

    BookController.java添加删除语句

--- src/main/java/com/itheima/controller/BookController.java
+++ src/main/java/com/itheima/controller/BookController.java
@@ -5,10 +5,7 @@ import com.itheima.controller.utile.R;
 import com.itheima.domain.Book;
 import com.itheima.service.IBookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/books")
@@ -20,6 +17,11 @@ public class BookController {
     public R getAll(){
 
         return new R(true,service.list());
+    }
+
+    @DeleteMapping("{id}")
+    public R delete(@PathVariable int id){
+        return new R(service.removeById(id));
     }
 
 
--- src/main/java/com/itheima/controller/utile/R.java
+++ src/main/java/com/itheima/controller/utile/R.java
@@ -22,4 +22,8 @@ public class R {
         this.flag = false;
         this.msg=msg;
     }
+
+    public R(Boolean flag) {
+        this.flag = flag;
+    }
 }
